fd2375de0e27fef3a0d752c7f9ed10048e66d881
isaacsabbath <isaacsabbath002@gmail.com>
2024-12-08 20:18:49 +0200

    cloud DB Config

--- app.py
+++ app.py
@@ -7,21 +7,18 @@ from werkzeug.utils import secure_filename
 from passlib.hash import sha256_crypt
 from functools import wraps
 from flask_wtf import FlaskForm
-hostname=myflaskapp.mysql.database.azure.com
-port=3306
-username=isaac
-password={your-password}
-ssl-mode=require
+from dotenv import load_dotenv
+
 
 
 app = Flask(__name__)
 app.secret_key = 'secret123'
 #Config mySQL
 
-app.config['MYSQL_HOST'] = 'myflaskapp.mysql.database.azure.com'
-app.config ['MYSQL_USER'] = 'isaac'
-app.config['MYSQL_PASSWORD'] = 'Richard002'
-app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_HOST'] = os.getenv('myflaskapp.mysql.database.azure.com')
+app.config ['MYSQL_USER'] = os.getenv('isaac')
+app.config['MYSQL_PASSWORD'] = os.getenv('Richard002')
+app.config['MYSQL_DB'] = os.getenv('myflaskapp')
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Initialise mysql
